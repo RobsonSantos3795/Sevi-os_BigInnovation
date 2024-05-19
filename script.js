d2990f87ceace9f0e7b7158dd3af06872359cf76
Robson G. <123911094+RobsonSantos3795@users.noreply.github.com>
2024-05-18 23:00:56 -0300

    interação botão com script

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function logar() {
     location.href = "index2.html";
 }
+
+var a = window.document.getElementById('area');
+
+a.addEventListener('mouseenter', entrar);
+a.addEventListener('mouseout', sair);
+
+function entrar() {
+    a.style.background = 'green';
+}
+
+function sair() {
+    a.style.background = 'yellow';
+}
